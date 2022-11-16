46534a666c7a2da26b67aa476f03a07bce436584
Omar <om300019@gmail.com>
2022-11-16 15:22:59 +0200

    delete never used variables

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,31 +8,6 @@ import Projects from "../Projects/Projects";
 import Skills from "../Skills/Skills";
 
 function Home() {
-  //Styles
-  const text_info = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    width: "100%",
-    transform: "translate(-50%, -50%)",
-    // backgroundColor: "rgba(255, 255, 255, 0.103)",
-    borderRadius: "20px",
-    padding: "25px",
-    display: "flex",
-    flexDirection: "column",
-    gap: "50px",
-    justifyContent: "center",
-    alignItems: "center",
-  };
-  const note = {
-    color: "white",
-    width: "fit-content",
-    backgroundColor: "rgba(0,0,0, 0.803)",
-    padding: "10px",
-    borderRadius: "10px",
-    fontSize: "15px",
-  };
-
   return (
     <div id="sidebar">
       <Sidebar pageWrapId={"home"} outerContainerId={"sidebar"} />
