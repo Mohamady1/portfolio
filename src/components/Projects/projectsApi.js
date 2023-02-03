1f6935d0a7acd2cf94e688aac872d32f335173c9
Omar <om300019@gmail.com>
2023-02-04 01:52:05 +0200

    add todo app

--- src/components/Projects/projectsApi.js
+++ src/components/Projects/projectsApi.js
@@ -1,6 +1,7 @@
 import weather from "../../img/Projects/weather.png";
 import musicPlayer from "../../img/Projects/musicPlayer.jpg";
 import weatherFlutter from "../../img/Projects/weather_flutter.jpg";
+import toDo from "../../img/Projects/to_do.png";
 
 const projects = [
   {
@@ -25,6 +26,14 @@ const projects = [
     name: "Flutter Weather App",
     picture: weatherFlutter,
     info: "Weather App With Flutter to know Weather, Wind Speed and Cloud Precentage for your city by location and for any city by search",
+    link: "",
+  },
+  {
+    id: 4,
+    technology: "Flutter",
+    name: "To-Do App",
+    picture: toDo,
+    info: "To-Do App With Flutter to make your day much more productive with flutter, provider and sqflite",
     link: "",
   },
 ];
